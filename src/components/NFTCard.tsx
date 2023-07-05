0a41a6c05cca4d98b2bacb04854c1880534beec9
Bob <maydaybob2000@gmail.com>
2023-07-05 22:15:29 +0800

    feat: basic UI and components setup

---
+++ src/components/NFTCard.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image';
+
+export function NFTCard() {
+  const title = "title"
+  const description = "description"
+
+  const imageNode = {
+    originalSrc: "https://fakeimg.pl/600x400/?text=Hello",
+    altText: "altText",
+  }
+  //
+  return (
+    <a className="h-120 w-72 rounded shadow-lg mx-auto border border-palette-lighter">
+      <div className="h-72 border-b-2 border-palette-lighter relative">
+        <Image
+          src={imageNode.originalSrc}
+          alt={imageNode.altText}
+          layout="fill"
+          className="transform duration-500 ease-in-out hover:scale-110"
+        />
+      </div>
+      <div className="h-48 relative">
+        <div className="font-primary text-palette-primary text-2xl pt-4 px-4 font-semibold">
+          {title}
+        </div>
+        <div className="text-lg text-gray-600 p-4 font-primary font-light">
+          Owner: {description}
+    
+        </div>
+
+        <div
+          className="text-palette-dark font-primary font-medium text-base absolute bottom-0 right-0 mb-4 pl-4 pr-4 pb-1 pt-2 bg-palette-lighter 
+            rounded-tl-sm triangle"
+        >
+          <form className="w-full max-w-sm">
+            <div className="flex items-center border-b border-teal-500 py-2">
+              <input
+                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                type="text"
+                placeholder="Jane Doe"
+                aria-label="Full name"
+              />
+              <button
+                className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                type="button"
+              >
+                Steal
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </a>
+  )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,5 @@
+import type { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <h1>123</h1>
+}
